3ec4555e35a56ed6f643dbd33b9ee25c2159504d
AntonDoroshenko <doroshenko.workmailbox@gmail.com>
2021-06-22 14:59:53 +0300

    Произвел работу над ошибками #2

--- src/homework2/task_2.py
+++ src/homework2/task_2.py
@@ -1,6 +1,6 @@
 sentence = input('Введите строку: ')
 for i in (',', '.', '!', '?', ':', ';'):
-    sentence = sentence.replace(i,'')
+    sentence = sentence.replace(i, '')
 sentence = sentence.split()
 Long_word = max(sentence, key=len)
 print('Самаое длинное слово это - ', Long_word)
--- src/homework2/task_5.py
+++ src/homework2/task_5.py
@@ -3,7 +3,7 @@ fib_1 = 0
 fib_2 = 1
 i = 1
 if i != number:
-    while i < number :
+    while i < number:
         fib_1, fib_2 = fib_2, fib_1 + fib_2
         i += 1
 print('Число Фибоначчи это ---> ', fib_1)
