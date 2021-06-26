b3a7145ca3a942afab7b2b07f6634d2e39337759
AntonDoroshenko <doroshenko.workmailbox@gmail.com>
2021-06-26 18:51:26 +0300

    Произвел работу над ошибками

--- src/homework3/task2.py
+++ src/homework3/task2.py
@@ -8,13 +8,13 @@
 # Одной строкой удалите элемент  '2a' из прошлого списка и напечатайте его.
 # Скопируйте список и добавьте в него элемент '2a' так
 # чтобы в исходном списке этого элемента не было.
-first_lst = a = [(i+j) for i in ['a', 'b'] for j in ['b', 'c', 'd']]
+first_lst = a = [(i + j) for i in ['a', 'b'] for j in ['b', 'c', 'd']]
 print(first_lst)
 
 second_lst = first_lst[::2]
 print(second_lst)
 
-numb_lst = [(i+j) for i in ['1', '2', '3', '4'] for j in ['a']]
+numb_lst = [(i + j) for i in ['1', '2', '3', '4'] for j in ['a']]
 print(numb_lst)
 
 print(numb_lst.pop(1))
